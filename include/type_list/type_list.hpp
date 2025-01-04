fd9ad3dc9ddfdb72beaef85c023a0100b4aebc14
pelectron <43130018+pelectron@users.noreply.github.com>
2025-01-04 13:56:03 +0100

    updated docs

--- include/type_list/type_list.hpp
+++ include/type_list/type_list.hpp
@@ -4,56 +4,6 @@
  * @version 1.0
  * @copyright Boost Software License - Version 1.0
  * @brief A single header C++ library for type lists.
- *
- * @mainpage
- *
- * type_list is a single header C++ library for type lists for C++11 and above.
- *
- * It features type trait like meta functions for any type list like type, i.e.
- * any type that only takes template type parameters. The meta functions
- * either have an inner typedef called type or a static constexpr member called
- * value, depending on whether the function returns a type or a value. Meta
- * functions returning a type have a shorthand alias with a '_t' suffix. From
- * C++14 onwards, meta functions returning a value also have a shorthand alias
- * with a  '_v' suffix.
- *
- * @section types Types
- *
- * Name | Description
- * ---- | -----------
- * ``TypeList<Ts...>`` | a "concrete" type list template with ``Ts`` as elements
- *
- * @section functions Meta functions
- *
- * @subsection values-functions Value Meta Functions
- *
- * Name                  | Description
- * --------------------- | ----------------------------------------------------------------------------------------------------
- * ``list_size<List>``   | returns the number of elements in ``List``
- * ``contains<T, List>`` | returns true if ``T`` is an element of ``List``
- * ``index_of<T, List>`` | returns the index of the first occurrence ``T`` in ``List``, or ``npos`` if ``T`` is not in ``List``
- *
- * @subsection type-functions Type Meta Functions
- *
- * Name                         | Description
- * ---------------------------- | ----------------------------------------------------------------------
- * ``head<List>``               | returns the first element in ``List``
- * ``tail<List>``               | returns ``List`` with the first element removed
- * ``first<List>``              | returns the first element in ``List``
- * ``last<List>``               | returns the last element in ``List``
- * ``type_at<N, List>``         | returns the ``N``-th type in ``List``
- * ``push_front<T, List>``      | returns ``List`` with ``T`` added at the front
- * ``push_back<T, List>``       | returns ``List`` with ``T`` added at the back
- * ``pop_front<List>``          | returns ``List`` with the first element removed
- * ``pop_back<List>``           | returns ``List`` with the last element removed
- * ``apply<F, List>``           | returns ``F<Ts...>`` where ``Ts`` are the elements of ``List``
- * ``extract<Class>``           | return the template parameters of ``Class`` as a type_list
- * ``for_each<F, List>``        | returns List with each element ``T`` replaced by ``F<T>``
- * ``transform<F, List>``       | returns List with each element ``T`` replaced by ``F<T>::type``
- * ``filter<Predicate, List>``  | returns ``List`` filtered with ``Predicate``
- * ``remove_duplicates<List>``  | removes duplicate occurrences in ``List``
- * ``merge<Lists...>``          | returns the elements of ``Lists`` concatenated into a single type list
- *
  */
 
 #ifndef TYPE_LIST_HPP
@@ -136,13 +86,34 @@ struct disjunction_helper<false, T, Ts...> : disjunction_helper<T::value, Ts...>
 
 
 template<class...Ts> struct disjunction : disjunction_helper<false, Ts...>{};
-  
+
+template <class FilteredList, class ToFilterList,
+          template <class> class Predicate>
+struct filter_impl;
+
+template <template <class...> class List1, template <class...> class List2,
+          class... Filtered, class ToFilter, class... ToBeFiltered,
+          template <class> class Predicate>
+struct filter_impl<List1<Filtered...>, List2<ToFilter, ToBeFiltered...>,
+                   Predicate> {
+  using type = typename std::conditional<
+      Predicate<ToFilter>::value,
+      typename filter_impl<List2<Filtered..., ToFilter>, List2<ToBeFiltered...>,
+                           Predicate>::type,
+      typename filter_impl<List2<Filtered...>, List2<ToBeFiltered...>,
+                           Predicate>::type>::type;
+};
+
+template <template <class...> class List1, template <class...> class List2,
+          class... Filtered, template <class> class Predicate>
+struct filter_impl<List1<Filtered...>, List2<>, Predicate> {
+  using type = List2<Filtered...>;
+};
 } // namespace
 
 /**
  * @brief get the first element of List.
- *
- * @tparam List
+ * @tparam List a type list
  */
 template <class List> struct head;
 
@@ -151,11 +122,14 @@ struct head<List<T, Ts...> > {
   using type = T;
 };
 
+/**
+ * @brief get the first element of List.
+ * @tparam List a type list
+ */
 template <class List> using head_t = typename head<List>::type;
 
 /**
  * @brief returns List with the first element removed.
- *
  * @tparam List a type list
  */
 template <class List> struct tail;
@@ -169,12 +143,15 @@ struct tail<List<T, Ts...> > {
   using type = List<Ts...>;
 };
 
+/**
+ * @brief returns List with the first element removed.
+ * @tparam List a type list
+ */
 template <class List> using tail_t = typename tail<List>::type;
 
 /**
  * @brief get the number of elements in a type list.
- *
- * @tparam List
+ * @tparam List a type list
  */
 template <class List> struct list_size;
 
@@ -188,7 +165,7 @@ struct list_size<List<Ts...> > {
  * npos if T is not in the List.
  *
  * @tparam T the type to search for
- * @tparam List the type list to search
+ * @tparam List a type list
  */
 template <class T, class List> struct index_of;
 
@@ -196,10 +173,9 @@ template <class T, template <class...> class List, class... Ts>
 struct index_of<T, List<Ts...> > : index_of_impl<0, T, Ts...> {};
 
 /**
- * @brief adds an element into the type list from the front.
- *
+ * @brief return List with T added to the front.
  * @tparam T the type to add
- * @tparam List the list to add onto
+ * @tparam List a type list
  */
 template <class T, class List> struct push_front;
 
@@ -208,13 +184,18 @@ struct push_front<T, List<Ts...> > {
   using type = TypeList<T, Ts...>;
 };
 
+/**
+ * @brief return List with T added to the front.
+ * @tparam T the type to add
+ * @tparam List a type list
+ */
 template <class T, class List>
 using push_front_t = typename push_front<T, List>::type;
 
 /**
- * @brief pop the first type from the list and return the shortend list.
- * @example pop_front<type_list<int,char> >::type == type_list<char>
- * @tparam List list pop the front of.
+ * @brief returns List with the first element removed. If List is empty, 
+ * an empty List is returned.
+ * @tparam List a type list
  */
 template <class List> struct pop_front;
 
@@ -227,31 +208,61 @@ template <template <class...> class List> struct pop_front<List<> > {
   using type = List<>;
 };
 
+/**
+ * @brief returns List with the first element removed. If List is empty, 
+ * an empty List is returned.
+ * @tparam List a type list
+ */
 template <class List> using pop_front_t = typename pop_front<List>::type;
 
 /**
- * @brief get N-th type in type_list List.
- *
- * @tparam N index of type
- * @tparam List
+ * @brief returns N-th type in type_list List.
+ * @tparam N integer index
+ * @tparam List a type list
  */
 template <size_t N, class List> struct type_at;
 
 template <size_t N, template <class...> class List, class... Ts>
 struct type_at<N, List<Ts...> > : type_at_impl<N, Ts...> {};
 
+/**
+ * @brief returns N-th type in type_list List.
+ * @tparam N integer index
+ * @tparam List a type list
+ */
 template <size_t N, class List>
 using type_at_t = typename type_at<N, List>::type;
 
-template <class List> using first_t = head_t<List>;
-template <class List>
-using last_t = type_at_t<list_size<List>::value - 1, List>;
+/**
+ * @brief returns the first element of List
+ * @tparam List a type list
+ */
+template <class List> using first = head<List>;
 
 /**
- * @brief add T to end of List.
- * @example push_back<double, type_list<int,char> >::type == type_list<int,
- * char, double>
- * @tparam List list to pop the front of.
+ * @brief returns the first element of List
+ * @tparam List a type list
+ */
+template <class List> using first_t = head_t<List>;
+
+/**
+ * @brief returns the last element of List
+ * @tparam List a type list
+ */
+template <class List>
+using last = type_at<list_size<List>::value - 1, List>;
+
+/**
+ * @brief returns the last element of List
+ * @tparam List a type list
+ */
+template <class List>
+using last_t = typename last<List>::type;
+
+/**
+ * @brief return List with T added at the end.
+ * @tparam T the element
+ * @tparam List a type list
  */
 template <class T, class List> struct push_back;
 
@@ -261,11 +272,9 @@ struct push_back<T, List<Ts...> > {
 };
 
 /**
- * @brief append T to the end of List.
- * @example push_back_t<double, type_list<int,char> > ==
- * type_list<int,char,double>
- * @tparam T
- * @tparam List
+ * @brief return List with T added at the end.
+ * @tparam T the element
+ * @tparam List a type list
  */
 template <class T, class List>
 using push_back_t = typename push_back<T, List>::type;
@@ -291,26 +300,23 @@ template <template <class...> class List> struct pop_back_impl<List<>, 0> {
 
 /**
  * @brief returns List with the last element removed.
- *
- * @tparam List
+ * @tparam List a type list
  */
 
 template <class List>
 struct pop_back : pop_back_impl<List, list_size<List>::value> {};
 
 /**
- * @brief pop last element of list and return the new list.
- *
- * @tparam List
+ * @brief returns List with the last element removed.
+ * @tparam List a type list
  */
 template <class List> using pop_back_t = typename pop_back<List>::type;
 
 /**
- * @brief applies the List to F, i.e. returns F<Ts...> where Ts are the elements
+ * @brief applies List to F, i.e. returns F<Ts...> where Ts are the elements
  * of List.
- *
  * @tparam F the type to apply to
- * @tparam List the list to apply
+ * @tparam List a type list
  */
 template <template <class...> class F, class List> struct apply;
 
@@ -320,11 +326,19 @@ struct apply<F, List<Ts...> > {
   using type = F<Ts...>;
 };
 
+/**
+ * @brief applies List to F, i.e. returns F<Ts...> where Ts are the elements
+ * of List.
+ * @tparam F the type to apply to
+ * @tparam List a type list
+ */
 template <template <class...> class F, class List>
 using apply_t = typename apply<F, List>::type;
 
 /**
- * @brief return the template parameters of Class as a type_list.
+ * @brief returns the template parameters of Class as a TypeList. If Class does
+ * not have template parameters, an empty list is returned.
+ * @note This does not work with non type template parameters.
  */
 template <class Class> struct extract;
 
@@ -333,12 +347,18 @@ struct extract<Class<Ts...> > {
   using type = TypeList<Ts...>;
 };
 
+/**
+ * @brief returns the template parameters of Class as a TypeList. If Class does
+ * not have template parameters, an empty list is returned.
+ * @note This does not work with non type template parameters.
+ * @tparam Class any type
+ */
 template <class Class> using extract_t = typename extract<Class>::type;
 
 /**
  * @brief replaces T with F<T> for each T in List.
- *
- * @tparam List the type list to iterate
+ * @tparam F a meta function returning true or false
+ * @tparam List a type list
  */
 template <template <class> class F, class List> struct for_each;
 
@@ -347,13 +367,17 @@ struct for_each<F, List<Ts...> > {
   using type = List<F<Ts>...>;
 };
 
+/**
+ * @brief replaces T with F<T> for each T in List.
+ * @tparam F a meta function returning true or false
+ * @tparam List a type list
+ */
 template <template <class> class F, class List>
 using for_each_t = typename for_each<F, List>::type;
 
 /**
  * @brief replaces T with F<T>::type for each T in List.
- *
- * @tparam F a type trait like type
+ * @tparam F a meta function returning true or false
  * @tparam List a type list
  */
 template <template <class> class F, class List> struct transform;
@@ -367,9 +391,14 @@ template <template <class> class F, class List>
 using transform_t = typename transform<F, List>::type;
 
 /**
- * @brief merges the Lists into a single type list.
+ * @brief merges Lists into a single type list.
  *
- * @tparam Lists the type lists to merge
+ * If sizeof...(Lists) is 0 then an TypeList<> is returned.
+ * If a mixture of type list templates is used, e.g. mixing TypeList with
+ * std::variant and std::tuple, then the result will use the underlying template of
+ * the first element in Lists. 
+ *
+ * @tparam Lists a pack of type lists
  */
 template <class... Lists> struct merge;
 
@@ -394,54 +423,42 @@ template <> struct merge<> {
   using type = TypeList<>;
 };
 
+/**
+ * @brief merges Lists into a single type list.
+ *
+ * If sizeof...(Lists) is 0 then an TypeList<> is returned.
+ * If a mixture of type list templates is used, e.g. mixing TypeList with
+ * std::variant and std::tuple, then the result will use the template of
+ * the first element in Lists. 
+ *
+ * @tparam Lists a pack of type lists
+ */
 template <class... Lists> using merge_t = typename merge<Lists...>::type;
 
-template <class FilteredList, class ToFilterList,
-          template <class> class Predicate>
-struct filter_impl;
-
-template <template <class...> class List1, template <class...> class List2,
-          class... Filtered, class ToFilter, class... ToBeFiltered,
-          template <class> class Predicate>
-struct filter_impl<List1<Filtered...>, List2<ToFilter, ToBeFiltered...>,
-                   Predicate> {
-  using type = typename std::conditional<
-      Predicate<ToFilter>::value,
-      typename filter_impl<List2<Filtered..., ToFilter>, List2<ToBeFiltered...>,
-                           Predicate>::type,
-      typename filter_impl<List2<Filtered...>, List2<ToBeFiltered...>,
-                           Predicate>::type>::type;
-};
-
-template <template <class...> class List1, template <class...> class List2,
-          class... Filtered, template <class> class Predicate>
-struct filter_impl<List1<Filtered...>, List2<>, Predicate> {
-  using type = List2<Filtered...>;
-};
 
 /**
- * @brief filters List with Predicate.
+ * @brief returns List filtered with Predicate.
  *
- * @tparam List
- * @tparam Predicate a type trait like type which contains a boolean member
- * value.
- * @example
- *  ```{.cpp}
- *  static_assert(filter<std::is_signed, type_list<int,unsigned, long, unsigned
- * short> >::type{} == type_list<int,long>{});
- *  ```
+ * @tparam Predicate a meta function returning true or false
+ * @tparam List a type list
  */
 template <template <class> class Predicate, class List>
 struct filter : filter_impl<TypeList<>, List, Predicate> {};
 
+/**
+ * @brief returns List filtered with Predicate.
+ *
+ * @tparam Predicate a meta function returning true or false
+ * @tparam List a type list
+ */
 template <template <class> class Predicate, class List>
 using filter_t = typename filter<Predicate, List>::type;
 
 /**
- * @brief returns true if T is an element of List.
+ * @brief returns true if T is an element of List, else false.
  *
  * @tparam T the type to search for
- * @tparam List the type list
+ * @tparam List a type list
  */
 template <class T, class List> struct contains;
 
@@ -471,13 +488,16 @@ struct remove_duplicates_impl<List1<Filtered...>, List2<> > {
 } // namespace
 
 /**
- * @brief removes duplicate occurences in List
- *
+ * @brief returns List with duplicate elements removed.
  * @tparam List a type list
  */
 template <class List>
 struct remove_duplicates : remove_duplicates_impl<TypeList<>, List> {};
 
+/**
+ * @brief returns List with duplicate elements removed.
+ * @tparam List a type list
+ */
 template <class List>
 using remove_duplicates_t = typename remove_duplicates<List>::type;
 
--- readme.md
+++ readme.md
@@ -2,26 +2,29 @@
 
 type_list is a header only C++ library for type lists written in C++11.
 
-type_list is a single header C++ library for type lists for C++11 and above.
-
 It features type trait like meta functions for any type list like type, i.e.
-any type that only takes template type parameters. The meta functions
-either have an inner typedef called type or a static constexpr member called
-value, depending on whether the function returns a type or a value. Meta
-functions returning a type have a shorthand alias with a '_t' suffix. From
-C++14 onwards, meta functions returning a value also have a shorthand alias
-with a  '_v' suffix.
+any type that only takes template type parameters (std::variant, std::tuple,
+etc.). The meta functions either have an inner typedef called type or a static
+constexpr member called value, depending on whether the function returns a type
+or a value. Meta functions returning a type have a shorthand alias with a '_t'
+suffix. From C++14 onwards, meta functions returning a value also have a
+shorthand alias with a  '_v' suffix.
 
-## Types
+## Installation
+
+Just clone the repo and add the include directory to your build
+system's include path.
+
+## API
 
 Name            | Description
 --------------- | ---------------------------------------------------
-``TypeList<Ts...>`` | a "concrete" type list template with ``Ts`` as elements
+``TypeList<Ts...>`` | a "concrete" type list template with elements ``Ts``
 
 ### Value Meta Functions
 
 Name                  | Description
---------------------- | ----------------------------------------------------------------------------------------------------
+--------------------- | ----------------------------------------------------------------------------
 ``list_size<List>``   | returns the number of elements in ``List``
 ``contains<T, List>`` | returns true if ``T`` is an element of ``List``
 ``index_of<T, List>`` | returns the index of the first occurrence ``T`` in ``List``, or ``npos`` if ``T`` is not in ``List``
@@ -49,11 +52,120 @@ Name                     | Description
 
 ## Examples
 
-TODO
+```cpp
+#include "type_list/type_list.hpp"
 
-## Installation
+namespace tl = type_list;
 
-None to do, just clone the repo and add the include directory to your build systems include path.
+using List = tl::TypeList<unsigned, int, long long, int, unsigned short>;
+
+template<typename T>
+struct Identity{
+  using type = T;
+};
+
+template<typename T>
+struct AlwaysTrue : std::true_type{};
+
+template<typename T>
+struct AlwaysFalse : std::false_type{};
+
+// list_size
+static_assert(tl::list_size_v<List> == 5);
+
+// contains
+static_assert(tl::contains_v<int, List>);
+static_assert(not tl::contains_v<char, List>);
+
+// index_of
+static_assert(tl::index_of_v<int, List> == 1);
+static_assert(tl::index_of_v<char, List> == tl::npos);
+
+// first
+static_assert(std::is_same_v<tl::first_t<List>, unsigned>);
+
+// last
+static_assert(std::is_same_v<tl::last_t<List>, unsigned short>);
+
+// head
+static_assert(std::is_same_v<tl::head_t<List>, unsigned>);
+
+// tail
+static_assert(std::is_same_v<tl::tail_t<List>,
+                             tl::TypeList<int, long long, int, unsigned short>>);
+
+// type_at
+static_assert(std::is_same_v<tl::type_at_t<3, List>, int>);
+// compilation error: static_assert(std::is_same_v<tl::type_at_t<5, List>, int>);
+
+// push_front
+static_assert(std::is_same_v<tl::push_front_t<char, List>,
+                             tl::TypeList<char, unsigned, int, long long,
+                                          int, unsigned short>>);
+
+// push_back
+static_assert(std::is_same_v<tl::push_back_t<char, List>,
+                             tl::TypeList<unsigned, int, long long, int, 
+                                          unsigned short, char>>);
+
+// pop_front
+static_assert(std::is_same_v<tl::pop_front_t<List>,
+                             tl::TypeList<int, long long, int, unsigned short>>);
+
+static_assert(std::is_same_v<tl::pop_front_t<tl::TypeList<>>, tl::TypeList<>>);
+
+// pop_back
+static_assert(std::is_same_v<tl::pop_back_t<List>,
+                             tl::TypeList<unsigned, int, long long, int>>);
+
+static_assert(std::is_same_v<tl::pop_back_t<tl::TypeList<>>, tl::TypeList<>>);
+
+// apply
+static_assert(std::is_same_v<tl::apply_t<std::variant, List>, 
+                             std::variant<unsigned, int, long long,
+                                          int, unsigned short>>);
+
+// extract
+static_assert(std::is_same_v<tl::extract_t<std::variant<unsigned, int,
+                                                        long long, int,
+                                                        unsigned short>>,
+                             List>);
+
+// for_each
+static_assert(std::is_same_v<tl::for_each_t<Identity, List>, 
+                             tl::TypeList<Identity<unsigned>, 
+                                          Identity<int>, 
+                                          Identity<long long>, 
+                                          Identity<int>, 
+                                          Identity<unsigned short>>>);
+
+// transform
+static_assert(std::is_same_v<tl::transform_t<Identity, List>, List>);
+static_assert(std::is_same_v<tl::transform_t<std::make_signed, List>,
+                             tl::TypeList<int, int, long long, int, short>>);
+
+// filter
+static_assert(std::is_same_v<filter_t<AlwaysFalse, List>, tl::TypeList<>>);
+static_assert(std::is_same_v<filter_t<AlwaysTrue, List>, List>);
+static_assert(std::is_same_v<filter_t<std::is_signed, List>,
+                             tl::TypeList<int, long long, int>>);
+
+// remove duplicates
+static_assert(std::is_same_v<remove_duplicates_t<List>, 
+                             tl::TypeList<unsigned, int, long long, unsigned short>>);
+
+// merge
+static_assert(std::is_same_v<merge_t<>, tl::TypeList<>>);
+
+static_assert(std::is_same_v<merge_t<List, List>, 
+                             tl::TypeList<unsigned, int, long long, int, 
+                                          unsigned short, unsigned, int, 
+                                          long long, int, unsigned short>>);
+
+static_assert(std::is_same_v<merge_t<std::variant<char,double>, List>, 
+                             std::variant<char, double, unsigned, int, 
+                                          long long, int, unsigned short>>);
+```
 
 ## Building Doxygen Docs
 
@@ -61,10 +173,14 @@ Building the html and man page documentation can be done with meson, or by hand.
 
 ### With meson
 
-1. setup the build directory: ``meson setup build``
-2. compile the docs target: ``meson compile docs -C build``
-3. the documentation is now available in the folder build/docs.
+1. Ensure that doxygen is in your path and cd into the repository.
+2. setup the build directory: ``meson setup build``
+3. compile the docs target: ``meson compile docs -C build``
+4. the documentation is now available in the folder ``build/docs``.
 
 ### By hand
 
-1. use the provided Doxyfile with doxygen. the environment variable INPUT must be set to ``include/type_list/type_list.hpp``, i.e. ``INPUT=include/type_list/type_list.hpp doxygen Doxyfile``.
+1. use the provided Doxyfile with doxygen. The environment variable ``SRCDIR`` must
+be set to root of this repository,
+e.g. ``SRCDIR=path/to/repo doxygen path/to/repo/Doxyfile``.
+4. the documentation is now available in the folder ``path/to/repo//docs``.
